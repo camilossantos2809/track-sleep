fce2d1590a4189679cb9756b84f0f2cf951813a1
camilossantos2809 <camilossantos.2809@gmail.com>
2021-01-17 18:30:14 -0300

    estrutura inicial do Room

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
@@ -16,3 +16,18 @@
 
 package com.example.android.trackmysleepquality.database
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "daily_sleep_quality_table")
+data class SleepNight(
+        @PrimaryKey(autoGenerate = true)
+        var nightId: Long = 0L,
+        @ColumnInfo(name = "start_milli")
+        val startMilli: Long = System.currentTimeMillis(),
+        @ColumnInfo(name = "end_time_milli")
+        var endTimeMilli: Long = startMilli,
+        @ColumnInfo(name = "quality_rating")
+        var sleepQuality: Int = -1
+)
